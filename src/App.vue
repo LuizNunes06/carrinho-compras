e331ccac9c0b079e0b7da0db920982bb8c819dcd
Luiz Felipe Nunes <106554495+LuizNunes06@users.noreply.github.com>
2023-05-23 11:59:50 -0300

    AAAAAAAAAAAAAAaa

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { livros} from '@/_data/livros'
+</script>
+
+<template>
+  <h1>Minha Livraria</h1>
+  <div class="listagem-livros">
+    <div v-for="livro in livros" :key="livro.id" class="card-livro">
+      <img :src="livro.cover" alt="capa" class="livro-capa">
+      <p class="livro-titulo">{{ livro.title }}</p>
+      <p class="livro-autor">{{ livro.author }}</p>
+      <p class="livro-preco">{{ livro.price }}</p>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+
+.card-livro{
+  background-color: rgb(245, 245, 199);
+  padding: 10px;
+  border-radius: 10px;
+  width: 200px;
+  margin: 10px;
+}
+.listagem-livros .livro-titulo{
+  font-weight: bold;
+  font-size: 1.2rem;
+  margin-bottom: 10px;
+}
+.listagem-livros{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+.listagem-livros p{
+  margin: 0;
+}
+  .livro-capa{
+    width: 200px;
+  }
+</style>
---
+++ src/_data/livros.js
@@ -0,0 +1,44 @@
+export const livros = [
+    {
+      id: 1,
+      title: "Eu, robo",
+      author: "Asimov",
+      price: 50.9,
+      cover: "https://m.media-amazon.com/images/I/414QJOKvD+L._SX328_BO1,204,203,200_.jpg"
+    },
+    {
+      id: 2,
+      title: "1984",
+      author: "Powell",
+      price: 42.0,
+      cover: "https://m.media-amazon.com/images/I/715OyML-v9L.jpg"
+    },
+    {
+      id: 3,
+      title: "O cavaleiro preso na armadura",
+      author: "Fisher",
+      price: 24.5,
+      cover: "https://m.media-amazon.com/images/I/91of8o2YfpL.jpg"
+    },
+    {
+      id: 4,
+      title: "O senhor dos aneis: A sociedade do anel",
+      author: "Tolkien",
+      price: 36.9,
+      cover: "https://cdn.shopify.com/s/files/1/0529/5125/3158/products/9786555113631.jpg?v=1678780395"
+    },
+    {
+      id: 5,
+      title: "Pequeno principe ",
+      author: "Saint-Exupéry",
+      price: 29.9,
+      cover: "https://m.media-amazon.com/images/I/81fXBeYYxpL.jpg"
+    },
+    {
+      id: 6,
+      title: "Fundacao",
+      author: "Asimov",
+      price: 49.9,
+      cover: "https://m.media-amazon.com/images/I/712vJIziMgL.jpg"
+    }
+  ]
